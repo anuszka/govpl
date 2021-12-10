de6cc1c2aaa1e1e42d4f83d5e50e84badd0cdb65
Anna Ochab-Marcinek <anna.ochab.marcinek@gmail.com>
2021-12-10 16:15:24 +0100

    Convert date to DateTime in Rogalski

--- share/rogalski_functions.py
+++ share/rogalski_functions.py
@@ -17,11 +17,11 @@ def format_Rogalski_voi(df : pd.DataFrame, start_row:int, end_row:int) -> pd.Dat
     dfc = df[start_row:end_row].copy(deep=True)
     dfc.iloc[0,1:304] = dfc.iloc[0,1:304] + '.2020'
     dfc.iloc[0,304:] = dfc.iloc[0,304:] + '.2021'
-    pd.to_datetime(dfc.iloc[0,1:], format='%d.%m.%Y')
     dfc.set_index(dfc.columns[0], inplace=True)
     dfc, dfc.columns = dfc[1:] , dfc.iloc[0]
     dfc.index.name = None
     dfc = dfc.apply(pd.to_numeric)
+    dfc.columns = pd.to_datetime(dfc.columns, format='%d.%m.%Y')
     return dfc
 
 # --------------------------------------------------------------------------    
